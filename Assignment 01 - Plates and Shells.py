ca7725ede02ee848ea1f381d8fd6923ef2ec07cb
Alexius Academia <alexius.academia@gmail.com>
2016-08-28 21:27:39 +0800

    Update Assignment 01 - Plates and Shells.py

--- Assignment 01 - Plates and Shells.py
+++ Assignment 01 - Plates and Shells.py
@@ -1,11 +1,12 @@
 #-------------------------#
 # Generalized Hooke's Law #
 #-------------------------#
+
+# To run this file, you must have sympy installed.
 import sympy as sm
-from sympy import init_printing
 
 # Create variable symbols
-v = sm.Symbol('v')              # Poison's ratio
+v = sm.Symbol('[v]')              # Poison's ratio
 E = sm.Symbol('E')              # Modulus of elasticity
 Qx = sm.Symbol('Qx')            # for sigma x
 Qy = sm.Symbol('Qy')            # for sigma y
@@ -30,6 +31,16 @@ V = sm.Matrix([E*EPx, E*EPy])   # Create the V matrix
 MInv = M**-1                    # Get the inverse matrix of M
 
 X = MInv * V                    # Solving for the X matrix
-
-print(sm.simplify(X[0]))        # Print the stress at x
-print(sm.simplify(X[1]))        # Print the stress at y
+Qx = sm.simplify(X[0])
+Qy = sm.simplify(X[1])
+print('\nMatrix M:')
+sm.pprint(M)
+print('\nMatrix V')
+sm.pprint(V)
+print('\nM*X = V, hence X = ')
+print(X)
+print('\nInterpretation of result:\n')
+print('\nQx:')
+sm.pprint(Qx)        # Print the stress at x
+print('\nQy:')
+sm.pprint(Qy)        # Print the stress at y
